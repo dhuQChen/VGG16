05aaa7689a2543d8403d518f07501f57fcaadd96
dhuQChen <2322740596@qq.com>
2019-05-30 19:58:16 +0800

    update

--- pre.py
+++ pre.py
@@ -2,13 +2,14 @@
 # Date: 2019.05.29
 import os
 import shutil
+import math
 
 dataDir = "./Data"
 files = os.listdir("./jpg")
 
 for file in files:
     index = (((file.split('.'))[0]).split('_'))[1]
-    C = int(index) // 81
+    C = math.ceil(int(index) / 80) - 1
     desDir = os.path.join(dataDir, str(C))
     if not os.path.exists(desDir):
         os.makedirs(desDir)
